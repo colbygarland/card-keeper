3cba93fc1cdbd5262d1cbd5150c6f790d30c45ea
Colby Garland <colbygarland@gmail.com>
2024-06-20 20:49:13 -0600

    track who went first

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Dispatch, SetStateAction, useState } from 'react'
 
 type Player = {
   scores: Array<number>
   theme: string
+  id: number
+  name: string
 }
 
 const EnterScore = ({
@@ -11,11 +14,13 @@ const EnterScore = ({
   name,
   updatePlayer,
   setReset,
+  setFirst,
 }: {
   player: Player
   name: string
   updatePlayer: Dispatch<SetStateAction<Player>>
   setReset: Dispatch<SetStateAction<boolean>>
+  setFirst: () => void
 }) => {
   const styles = {
     fuchsia: { text: 'text-fuchsia-600', focus: 'border-fuchsia-300 focus:ring-fuchsia-600', bg: 'bg-fuchsia-600' },
@@ -33,6 +38,7 @@ const EnterScore = ({
     e.target[0].value = null
 
     setReset(true)
+    setFirst()
   }
 
   return (
@@ -55,15 +61,19 @@ export default function Home() {
   const [player1, setPlayer1] = useState<Player>({
     scores: [],
     theme: 'fuchsia',
+    id: 1,
+    name: player1Name,
   })
   const [player2, setPlayer2] = useState<Player>({
     scores: [],
     theme: 'indigo',
+    id: 2,
+    name: player2Name,
   })
   const [showReset, setShowReset] = useState(false)
+  const [firstPlayer, setFirstPlayer] = useState<Player | null>(null)
   const player1Total = player1.scores.reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0)
   const player2Total = player2.scores.reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0)
-  const currentPlayer: Player = player1
   const title = "Paul's Card Tracker"
 
   const scores = []
@@ -75,6 +85,13 @@ export default function Home() {
     })
   }
 
+  const setFirst = (player: Player) => {
+    if (!firstPlayer) {
+      setFirstPlayer(player)
+      console.log('setting player', player)
+    }
+  }
+
   return (
     <main>
       <div className="p-6">
@@ -82,8 +99,8 @@ export default function Home() {
         <div className="">
           <div className="">
             <div className="grid grid-cols-2 gap-4">
-              <EnterScore name={player1Name} player={player1} updatePlayer={setPlayer1} setReset={setShowReset} />
-              <EnterScore name={player2Name} player={player2} updatePlayer={setPlayer2} setReset={setShowReset} />
+              <EnterScore name={player1Name} player={player1} updatePlayer={setPlayer1} setReset={setShowReset} setFirst={() => setFirst(player1)} />
+              <EnterScore name={player2Name} player={player2} updatePlayer={setPlayer2} setReset={setShowReset} setFirst={() => setFirst(player2)} />
             </div>
             <div className="mt-6">
               <h3 className="font-bold text-lg mb-4 text-gray-700">Round Score Tracker</h3>
@@ -91,22 +108,30 @@ export default function Home() {
                 <thead className="bg-gray-100">
                   <tr>
                     <th className="px-4 py-2 text-gray-600">Round</th>
-                    <th className={`px-4 py-2 text-${player1.theme}-600`}>{player1Name}</th>
-                    <th className={`px-4 py-2 text-${player2.theme}-600`}>{player2Name}</th>
+                    <th className={`px-4 py-2 text-${player1.theme}-600`}>
+                      {player1Name}
+                      {firstPlayer?.id === player1.id ? '🥇' : ''}
+                    </th>
+                    <th className={`px-4 py-2 text-${player2.theme}-600`}>
+                      {player2Name}
+                      {firstPlayer?.id === player2.id ? '🥇' : ''}
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   {scores.map((score, index) => {
                     return (
                       <tr key={index}>
-                        <td className="border px-4 py-2">{index + 1}</td>
+                        <td className="border px-4 py-2 bg-gray-100">
+                          <span className="font-bold">{index + 1}</span>
+                        </td>
                         <td className="border px-4 py-2">{score.player1 ?? '-'}</td>
                         <td className="border px-4 py-2">{score.player2 ?? '-'}</td>
                       </tr>
                     )
                   })}
                   <tr>
-                    <td className="border px-4 py-2">
+                    <td className="border px-4 py-2 bg-gray-100">
                       <strong>Total</strong>
                     </td>
                     <td className="border px-4 py-2">
@@ -120,10 +145,13 @@ export default function Home() {
               </table>
             </div>
             {showReset && (
-              <div className="mt-8">
+              <div className="mt-8 grid gap-4 grid-cols-2">
                 <a href="" className="font-bold block w-full px-2 py-1 text-sm mt-2 text-fuchsia-600 border-2 border-fuchsia-600 text-center rounded">
                   Reset
                 </a>
+                <Link href="/" className="font-bold block w-full px-2 py-1 text-sm mt-2 text-indigo-600 border-2 border-indigo-600 text-center rounded">
+                  New Names!
+                </Link>
               </div>
             )}
           </div>
